2a2b85b3e3df04bac9cf63c33dae955b530923c7
Mikko Viitamäki <mikko.viitamaki@logisnext.eu>
2025-01-08 22:19:08 +0200

    use totp-embed to calculate pin number, #no-std

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,17 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+use totp_embed::{totp, totp_custom, Sha512};
 fn main() {
-    println!("Hello, world!");
+
+// Negotiated between you and the authenticating service.
+let password: &[u8] = b"top secret";
+
+// The number of seconds since the Unix Epoch.
+let seconds: u64 = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+
+// Specify the desired Hash algorithm via a type parameter.
+// `Sha1` and `Sha256` are also available.
+let result: u64 = totp::<Sha512>(password, seconds);
+let r =   totp_custom::<Sha512>(30,6, password, seconds);
+println!("code: {result}");
+println!("code: {r}");
 }
